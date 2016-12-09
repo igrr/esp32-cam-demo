a95145437152c00c5f9f2c2add33c2eb0a1c86c9
Oitzu <blackoise@googlemail.com>
2016-12-09 23:39:11 +0100

    Add ov2640 support files.

--- components/camera/ov7725.c
+++ components/camera/ov7725.c
@@ -48,7 +48,7 @@ static const uint8_t default_regs[][2] = {
     {COM8,          0xF0},
     {COM6,          0xC5},
     {COM9,          0x11},
-    {COM10,         (1 << 1) | (1 << 5)},
+    {COM10,         COM10_VSYNC_NEG}, //Invert VSYNC
     {BDBASE,        0x7F},
     {DBSTEP,        0x03},
     {AEW,           0x96},
