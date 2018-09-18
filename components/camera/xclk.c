14b6170060d03847ece370013be7448b25be1f19
Ivan Grokhotkov <ivan@espressif.com>
2018-09-18 17:55:32 +0800

    fix xclk for latest IDF

--- components/camera/xclk.c
+++ components/camera/xclk.c
@@ -10,24 +10,26 @@ esp_err_t camera_enable_out_clock(camera_config_t* config)
 {
     periph_module_enable(PERIPH_LEDC_MODULE);
 
-    ledc_timer_config_t timer_conf;
-    timer_conf.duty_resolution = 1;
-    timer_conf.freq_hz = config->xclk_freq_hz;
-    timer_conf.speed_mode = LEDC_HIGH_SPEED_MODE;
-    timer_conf.timer_num = config->ledc_timer;
+    ledc_timer_config_t timer_conf = {
+        .duty_resolution = 1,
+        .freq_hz = config->xclk_freq_hz,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = config->ledc_timer
+    };
     esp_err_t err = ledc_timer_config(&timer_conf);
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "ledc_timer_config failed, rc=%x", err);
         return err;
     }
 
-    ledc_channel_config_t ch_conf;
-    ch_conf.channel = config->ledc_channel;
-    ch_conf.timer_sel = config->ledc_timer;
-    ch_conf.intr_type = LEDC_INTR_DISABLE;
-    ch_conf.duty = 1;
-    ch_conf.speed_mode = LEDC_HIGH_SPEED_MODE;
-    ch_conf.gpio_num = config->pin_xclk;
+    ledc_channel_config_t ch_conf = {
+        .channel = config->ledc_channel,
+        .timer_sel = config->ledc_timer,
+        .intr_type = LEDC_INTR_DISABLE,
+        .duty = 1,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .gpio_num = config->pin_xclk
+    };
     err = ledc_channel_config(&ch_conf);
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "ledc_channel_config failed, rc=%x", err);
